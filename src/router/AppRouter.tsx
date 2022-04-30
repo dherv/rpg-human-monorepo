d0a22d5bc56c6d35c05fbc7e9fe3e5146a79ee19
dherv <damien.herv@gmail.com>
2022-04-30 12:45:15 +0900

    feat: add error boundary

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from '../App';
+import ErrorBoundary from '../app/ErrorBoundary';
 import { SessionGraph } from '../components/base/SessionGraph';
 import { SessionLogs } from '../components/base/SessionLogs';
 import { NotFound } from '../components/pages/NotFound';
@@ -26,39 +27,41 @@ const SessionAddPage = React.lazy(() =>
 export const AppRouter: FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="sessions">
-            <Route index element={<SessionLogs />}></Route>
-            <Route path="graph" element={<SessionGraph />}></Route>
+      <ErrorBoundary>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route path="sessions">
+              <Route index element={<SessionLogs />}></Route>
+              <Route path="graph" element={<SessionGraph />}></Route>
+            </Route>
+            <Route
+              path="activities"
+              element={
+                <React.Suspense fallback={<>...</>}>
+                  <ActivityContainer />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="character"
+              element={
+                <React.Suspense fallback={<>...</>}>
+                  <Character />
+                </React.Suspense>
+              }
+            />
+            <Route
+              path="new-session"
+              element={
+                <React.Suspense fallback={<>...</>}>
+                  <SessionAddPage />
+                </React.Suspense>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
           </Route>
-          <Route
-            path="activities"
-            element={
-              <React.Suspense fallback={<>...</>}>
-                <ActivityContainer />
-              </React.Suspense>
-            }
-          />
-          <Route
-            path="character"
-            element={
-              <React.Suspense fallback={<>...</>}>
-                <Character />
-              </React.Suspense>
-            }
-          />
-          <Route
-            path="new-session"
-            element={
-              <React.Suspense fallback={<>...</>}>
-                <SessionAddPage />
-              </React.Suspense>
-            }
-          />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+        </Routes>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
