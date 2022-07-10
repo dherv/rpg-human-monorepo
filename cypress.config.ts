6cd9a2cd35cc5c64e1e579cd8df2059491a26511
dherv <34971414+dherv@users.noreply.github.com>
2022-07-10 14:57:29 +0900

    chore(ci): add gitlab yaml file and jobs (#4)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: 'r1fbqo',
   e2e: {
     // setupNodeEvents(on, config) {
     //   // implement node event listeners here
