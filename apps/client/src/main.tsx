e0b3c7b5afbf9a3f38dc36bbca3d27d255e15694
dherv <damien.herv@gmail.com>
2022-11-23 20:20:38 +0900

    chore: add material ui and joy ui kit

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,4 +1,8 @@
 import '@fontsource/public-sans'
+import colors from '@mui/joy/colors'
+import { CssVarsProvider, extendTheme as extendJoyTheme } from '@mui/joy/styles'
+import { experimental_extendTheme as extendMuiTheme } from '@mui/material/styles'
+import { deepmerge } from '@mui/utils'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
@@ -11,10 +15,83 @@ if (process.env.NODE_ENV === 'development') {
   worker.start()
 }
 
+const muiTheme = extendMuiTheme({
+  // This is required to point to `var(--joy-*)` because we are using `CssVarsProvider` from Joy UI.
+  cssVarPrefix: 'joy',
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: colors.blue[500],
+        },
+        grey: colors.grey,
+        error: {
+          main: colors.red[500],
+        },
+        info: {
+          main: colors.purple[500],
+        },
+        success: {
+          main: colors.green[500],
+        },
+        warning: {
+          main: colors.yellow[200],
+        },
+        common: {
+          white: '#FFF',
+          black: '#09090D',
+        },
+        divider: colors.grey[200],
+        text: {
+          primary: colors.grey[800],
+          secondary: colors.grey[600],
+        },
+      },
+    },
+    dark: {
+      palette: {
+        primary: {
+          main: colors.blue[600],
+        },
+        grey: colors.grey,
+        error: {
+          main: colors.red[600],
+        },
+        info: {
+          main: colors.purple[600],
+        },
+        success: {
+          main: colors.green[600],
+        },
+        warning: {
+          main: colors.yellow[300],
+        },
+        common: {
+          white: '#FFF',
+          black: '#09090D',
+        },
+        divider: colors.grey[800],
+        text: {
+          primary: colors.grey[100],
+          secondary: colors.grey[300],
+        },
+      },
+    },
+  },
+})
+
+const joyTheme = extendJoyTheme()
+
+// You can use your own `deepmerge` function.
+// joyTheme will deeply merge to muiTheme.
+const theme = deepmerge(muiTheme, joyTheme)
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppRouter />
+      <CssVarsProvider theme={theme}>
+        <AppRouter />
+      </CssVarsProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
