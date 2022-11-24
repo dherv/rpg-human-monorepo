dd26a5807c126f33e0437579477628c02fe3f398
dherv <34971414+dherv@users.noreply.github.com>
2022-11-24 18:53:51 +0900

    refactor: RH-48 navbar mobile and desktop (#3)

--- apps/client/src/components/layout/Nav.tsx
+++ apps/client/src/components/layout/Nav.tsx
@@ -1,37 +1,93 @@
-import { AdjustmentsIcon, MenuAlt2Icon, PlusIcon } from '@heroicons/react/outline'
-import { FC } from 'react'
-import { CustomLink } from './CustomLink'
+import { PersonPin, SsidChart, Terrain } from '@mui/icons-material'
+import { Tab, TabList, Tabs, Typography } from '@mui/joy'
+import { styled, useTheme } from '@mui/joy/styles'
+import { tabClasses } from '@mui/joy/Tab'
+import { typographyClasses } from '@mui/joy/Typography'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { FC, SyntheticEvent } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 export const Nav: FC = () => {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const theme = useTheme()
+  const matches = useMediaQuery(theme.breakpoints.up('md'))
+
+  const handleChange = (
+    _event: SyntheticEvent<Element, Event>,
+    value: string | number | boolean,
+  ) => {
+    if (typeof value === 'string') {
+      navigate(value)
+    }
+  }
+
+  const Root = styled('div')(({ theme }) => ({
+    position: 'fixed',
+    top: 'auto',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    borderTop: `1px solid ${theme.colorSchemes.light.palette.divider}`,
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 85,
+      top: 0,
+      bottom: 0,
+      right: 'auto',
+      borderRight: `1px solid ${theme.colorSchemes.light.palette.divider}`,
+    },
+  }))
   return (
-    <nav>
-      <ul className='flex justify-between items-center text-sm p-2 -ml-2'>
-        <div className='flex'>
-          <li className='mx-2'>
-            <CustomLink to='/sessions'>sessions</CustomLink>
-          </li>
-          <li className='mx-2'>
-            <CustomLink to='/activities'>activities</CustomLink>
-          </li>
-        </div>
-        <div className='flex'>
-          <li className='mx-2'>
-            <CustomLink to='/sessions/graph'>
-              <MenuAlt2Icon className='h-4 w-4 text-gray-600' />
-            </CustomLink>
-          </li>
-          <li className='mx-2' data-cy='session-add'>
-            <CustomLink to='/new-session'>
-              <PlusIcon className='h-4 w-4 text-gray-600' />
-            </CustomLink>
-          </li>
-          <li className='mx-2'>
-            <CustomLink to='/sessions/graph'>
-              <AdjustmentsIcon className='h-4 w-4 text-gray-600' />
-            </CustomLink>
-          </li>
-        </div>
-      </ul>
-    </nav>
+    <Root>
+      <Tabs
+        aria-label='Icon tabs'
+        defaultValue={0}
+        size='lg'
+        orientation={matches ? 'vertical' : 'horizontal'}
+        onChange={handleChange}
+        value={location.pathname}
+        sx={{
+          width: '100%',
+          mx: 0,
+        }}
+      >
+        <TabList
+          sx={{
+            bgcolor: 'transparent',
+            [`& .${tabClasses.root}`]: {
+              flexDirection: 'column',
+              bgcolor: 'transparent',
+              boxShadow: 'none',
+              opacity: 0.5,
+              '&:hover': {
+                bgcolor: 'transparent',
+              },
+              [`&.${tabClasses.selected}`]: {
+                color: '#000',
+                opacity: 1,
+                fontWeight: 'lg',
+                [`& .${typographyClasses.root}`]: {
+                  color: '#000',
+                },
+              },
+            },
+          }}
+        >
+          <Tab value='/activities'>
+            <Terrain />
+            <Typography level='body5'>activities</Typography>
+          </Tab>
+          <Tab value='/sessions'>
+            <SsidChart />
+            <Typography level='body5'>sessions</Typography>
+          </Tab>
+          <Tab value='/character'>
+            <PersonPin />
+            <Typography level='body5'>character</Typography>
+          </Tab>
+        </TabList>
+      </Tabs>
+    </Root>
   )
 }
